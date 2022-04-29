b74021408b571e70282ec03df9c38d3a2101db72
Daniel Rossa <danielrossa92@gmail.com>
2022-04-29 18:29:22 +0200

    iterator is name of dynamic block

--- vm_cloud-main.tf
+++ vm_cloud-main.tf
@@ -97,7 +97,7 @@ resource "proxmox_vm_qemu" "vm" {
       model         = "e1000"
       bridge        = network.value.net_bridge
       tag           = network.value.net_vlan
-      macaddr       = macaddress.vm_macaddress[each.key].address
+      macaddr       = macaddress.vm_macaddress[network].address
     }
   }
 

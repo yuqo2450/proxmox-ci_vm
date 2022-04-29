006a852a3e2bd6836335457f62daa8edfd934410
Daniel Rossa <danielrossa92@gmail.com>
2022-04-29 11:22:49 +0200

    this is the base vm module repository that should be copied

---
+++ vm_cloud-main.tf
@@ -0,0 +1,104 @@
+terraform {
+  ### Add required providers here.
+  required_providers {
+    telmate-proxmox = {
+      source  = "telmate/proxmox"
+      version = ">= 2.7.1"
+    }
+    bpg-proxmox = {
+      source  = "bpg/proxmox"
+      version = ">= 0.4.4"
+    }
+    macaddress = {
+      source = "ivoronin/macaddress"
+      version = ">= 0.3.0"
+    }
+  }
+}
+
+### Add values u want to reuse in this file. Or shorten expressions.
+locals {
+  macaddress = upper(macaddress.vm_macaddress.address)
+  civars = merge(var.cloudinit_vars, {"mac_address" = local.macaddress})
+  userdata = proxmox_virtual_environment_file.vm_userdata.id
+  netdata = proxmox_virtual_environment_file.netdata.id
+}
+
+### Add resources in this section.
+resource "macaddress" "vm_macaddress" { }
+
+resource "proxmox_virtual_environment_file" "vm_userdata" {
+  
+  provider      = bpg-proxmox
+
+  content_type  = "snippets"
+  datastore_id  = "local"
+  node_name     = var.node_name
+
+  source_raw {
+    data      = templatefile(var.userdata_template, local.civars)
+    file_name = "${var.vm_name}-user.yaml"
+  }
+}
+
+resource "proxmox_virtual_environment_file" "vm_netdata" {
+  
+  provider      = bpg-proxmox
+
+  content_type  = "snippets"
+  datastore_id  = "local"
+  node_name     = var.node_name
+
+  source_raw {
+    data      = templatefile(var.netdata_template, local.civars)
+    file_name = "${var.vm_name}-net.yaml"
+  }
+}
+
+resource "proxmox_vm_qemu" "vm" {
+
+  provider          = telmate-proxmox
+
+  name              = var.vm_name
+  desc              = var.description
+  vmid              = var.vm_id
+
+  target_node       = var.node_name
+  clone             = var.template
+  agent             = 1
+
+  os_type           = "cloud-init"
+  cores             = var.cpu_cores
+  sockets           = 1
+  vcpus             = var.cpu_vcpu
+  numa              = true
+  cpu               = "qemu64"
+  memory            = var.mem_size
+  balloon           = 512
+  hotplug           = "network,disk,usb,memory,cpu"
+  qemu_os           = "l26"
+  scsihw            = "virtio-scsi-pci"
+
+  dynamic "disk" {
+    for_each        = var.vm_disks[*]
+    content {
+      size          = disk.value.disk_size
+      type          = "scsi"
+      storage       = disk.value.disk_datastore
+    }
+  }
+
+  bootdisk          = "scsi0"
+
+  dynamic "network" {
+    for_each        = var.vm_networks[*]
+    content {
+      model         = "e1000"
+      bridge        = network.value.net_bridge
+      tag           = network.value.net_vlan
+      macaddr       = local.macaddress
+    }
+  }
+
+  cicustom          = "user=${local.userdata},network=${local.netdata}"
+}
---
+++ vm_cloud-variables.tf
@@ -0,0 +1,95 @@
+variable "vm_name" {
+  type  = string
+  description = "The name of the virtual machine."
+}
+
+variable "vm_id" {
+  type = number
+  description = "VM ID to be used."
+  default = null
+}
+variable "node_name" {
+  type  = string
+  description = "Name of the node to deploy vm to."
+}
+
+variable "template" {
+  type = string
+  description = "Name of the vm or template to clone."
+}
+
+variable "cpu_cores" {
+  type = number
+  default = 4
+}
+
+variable "cpu_vcpu" {
+  type = number
+  default = 2
+}
+
+variable "mem_size" {
+  type = number
+  default = 1024
+}
+
+variable "vm_disks" {
+  type = list(object(
+    {
+      disk_datastore = string
+      disk_size = string
+    }
+  ))
+  default = [
+    {
+      disk_datastore = "local-zfs"
+      disk_size = "10G"
+    }
+  ]
+}
+
+variable "vm_networks" {
+  type = list(object(
+    {
+      net_bridge = string
+      net_vlan = number
+    } 
+  ))
+  default = [
+    {
+      net_bridge = "vmbr0"
+      net_vlan = -1
+    }
+  ]
+}
+
+variable "description" {
+  type = string
+  default = "cloud-init vm"
+  description = "Short text to give overview of the vm usage."
+}
+
+variable "userdata_template" {
+  type = string
+  description = "Path to cloud init user-data template file on local filesystem."
+}
+
+variable "netdata_template" {
+  type = string
+  description = "Path to cloud init network-data template file on local filesystem."
+}
+
+variable "cloudinit_vars" {
+  type = object({
+    hostname = string
+    domain = string
+    ip4_addresses = list(string)
+    ip4_gateway = string
+    ip6_addresses = list(string)
+    ip6_gateway = string
+    dnsservers = list(string)
+    searchdomains = list(string)
+  })
+  default = null
+  description = "Host specific network data values (macaddress)."
+}

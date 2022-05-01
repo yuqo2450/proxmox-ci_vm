141d21d4f0bf9f86422e16811fa8da7895a03566
Daniel Rossa <danielrossa92@gmail.com>
2022-05-01 10:29:05 +0200

    changed resource name

--- vm_base-main.tf
+++ vm_base-main.tf
@@ -18,7 +18,7 @@ terraform {
 
 ### Add values u want to reuse in this file. Or shorten expressions.
 locals {
-  networks = {for k, v in var.vm_networks : k => merge(v, {"mac_address" = macaddress.vm_macaddress[regex("\\d",k)].address})}
+  networks = {for k, v in var.vm_networks : k => merge(v, {"mac_address" = macaddress.vm_base_macaddress[regex("\\d",k)].address})}
   userdata = proxmox_virtual_environment_file.vm_base_userdata.id
   netdata = proxmox_virtual_environment_file.vm_base_netdata.id
 }

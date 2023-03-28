8b2e478885e1e982b248503b27d98da8e178d141
Daniel Rossa <danielrossa92@gmail.com>
2023-03-28 13:17:52 +0200

    local.startup defaults to null if var.startup is null

--- locals.tf
+++ locals.tf
@@ -3,5 +3,5 @@ locals {
   interfaces = { for k, v in var.interfaces : k => merge(v, { "mac_address" = macaddress.vm_base_macaddress[regex("\\d", k)].address }) }
   userdata   = merge(var.userdata_vars, { "hostname" = var.name })
   netdata    = merge(var.netdata_vars, { "interfaces" = local.interfaces })
-  startup    = join(",", [for key, value in var.startup : "${key}=${value}" if value != null])
+  startup    = var.startup != null ? join(",", [for key, value in var.startup : "${key}=${value}" if value != null]) : null
 }

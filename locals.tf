c2e1cb931b4bd3413f80081d19cbc2b9f8634871
Daniel Rossa <danielrossa92@gmail.com>
2023-03-28 10:53:00 +0200

    linted code

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 
 locals {
-  interfaces = { for k, v in var.interfaces : k => merge(v, { "mac_address" = macaddress.vm_base_macaddress[regex("\\d", k)].address }) } 
+  interfaces = { for k, v in var.interfaces : k => merge(v, { "mac_address" = macaddress.vm_base_macaddress[regex("\\d", k)].address }) }
   userdata   = merge(var.userdata_vars, { "hostname" = var.name })
   netdata    = merge(var.netdata_vars, { "interfaces" = local.interfaces })
 }
